e44dd1ce08e054e6cdf4189c1feae5525606a95a
Николай <programmer.vip.pro@gmail.com>
2023-05-23 13:19:38 +0300

    Создание окна регистрации. Часть 1

--- Flowers/Form1.Designer.cs
+++ Flowers/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Flowers
 {
-    partial class Form1
+    partial class I_Can_Buy_Myself_Flowers
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.NameText = new System.Windows.Forms.TextBox();
+            this.FFF = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(558, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 62);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Регистрация";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = global::Flowers.Properties.Resources.logt;
+            this.pictureBox1.Location = new System.Drawing.Point(629, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 133);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // NameText
+            // 
+            this.NameText.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.NameText.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.NameText.Location = new System.Drawing.Point(646, 242);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(100, 23);
+            this.NameText.TabIndex = 2;
+            this.NameText.Text = "Имя";
+            this.NameText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.NameText.Enter += new System.EventHandler(this.NameText_Enter);
+            this.NameText.Leave += new System.EventHandler(this.NameText_Leave);
+            // 
+            // FFF
+            // 
+            this.FFF.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FFF.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.FFF.Location = new System.Drawing.Point(646, 297);
+            this.FFF.Name = "FFF";
+            this.FFF.Size = new System.Drawing.Size(100, 23);
+            this.FFF.TabIndex = 3;
+            this.FFF.Text = "Фамилия";
+            this.FFF.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.FFF.Enter += new System.EventHandler(this.FFF_Enter);
+            this.FFF.Leave += new System.EventHandler(this.FFF_Leave);
+            // 
+            // I_Can_Buy_Myself_Flowers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1457, 590);
+            this.Controls.Add(this.FFF);
+            this.Controls.Add(this.NameText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "I_Can_Buy_Myself_Flowers";
+            this.Text = "I Can Buy Myself Flowers";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private Label label1;
+        private PictureBox pictureBox1;
+        public TextBox NameText;
+        public TextBox FFF;
     }
 }
--- Flowers/Form1.cs
+++ Flowers/Form1.cs
@@ -1,10 +1,75 @@
 namespace Flowers
 {
-    public partial class Form1 : Form
+    public partial class I_Can_Buy_Myself_Flowers : Form
     {
-        public Form1()
+        public I_Can_Buy_Myself_Flowers()
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.logt;
+        }
+
+        private void I_Can_Buy_Myself_Flowers_Load(object sender, EventArgs e)
+        {
+
+        }
+
+     
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        
+
+        private void NameText_Enter(object sender, EventArgs e)
+        {
+            NameText.Cursor = Cursors.No;
+            if (NameText.Text == "Имя")
+            {
+                NameText.Text = "";
+
+                NameText.ForeColor = Color.Black;
+            }
+        }
+        private void NameText_Leave(object sender, EventArgs e)
+        {
+            if (NameText.Text == "")
+            {
+                NameText.Text = "Имя";
+
+                NameText.ForeColor = Color.Gray;
+            }
+        }
+
+
+
+        private void FFF_Enter(object sender, EventArgs e)
+        {
+
+            if (FFF.Text == "Фамилия")
+            {
+                FFF.Text = "";
+
+                FFF.ForeColor = Color.Black;
+            }
+        }
+
+        private void FFF_Leave(object sender, EventArgs e)
+        {
+            if (FFF.Text == "")
+            {
+                FFF.Text = "Фамилия";
+
+                FFF.ForeColor = Color.Gray;
+            }
+        }
+
     }
 }
